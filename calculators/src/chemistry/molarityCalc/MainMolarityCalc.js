ce854e53e0aba6a27c761f297e78b5c94a9199bc
Ishaan Shettigar <ishaanshettigar@gmail.com>
2023-10-30 00:15:07 +0530

    half done molarity calc

--- calculators/src/chemistry/molarityCalc/MainMolarityCalc.js
+++ calculators/src/chemistry/molarityCalc/MainMolarityCalc.js
@@ -1,17 +1,45 @@
-import React from 'react';
-import { Container, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import { Container, Typography, Tab, Tabs, Box, TextField, InputAdornment } from '@mui/material';
 
-function MainMolarityCalc(){
-    return(
-        <Container maxWidth="lg" sx={{ bgcolor: '#eeeeee', minHeight: '90vh', paddingY:"10" }}>
-            <Typography pt={1} variant='h5' sx = {{textAlign: "center"}}>Molarity Calculator</Typography>
-            <hr/>
-            <br/>
-            {/* Write your code here */}
+function MainMolarityCalc() {
+    const [currentTabIndex, setCurrentTabIndex] = useState(0);
+    const [moles, setMoles] = useState(0);
+    const [molarity, setMolarity] = useState(0);
+    const [soluteVolume, setSoluteVolume] = useState(0);
 
 
+    const handleTabChange = (e, tabIndex) => {
+        console.log(tabIndex);
+        setCurrentTabIndex(tabIndex);
+    };
 
-            {/* End your code here */}
+
+    return (
+        <Container maxWidth="lg" sx={{ bgcolor: '#eeeeee', minHeight: '90vh', paddingY: "10" }}>
+            <Typography pt={1} variant='h5' sx={{ textAlign: "center" }}>Molarity Calculator</Typography>
+            <hr />
+            <br />
+
+            <Container maxWidth="sm">
+                <Tabs value={currentTabIndex} onChange={handleTabChange} sx={{ marginBottom: "2.5em" }}>
+                    <Tab label='Tab 1' />
+                    <Tab label='Tab 2' />
+                </Tabs>
+                {/* TAB 1 Contents */}
+                {currentTabIndex === 0 && (
+                    <Box sx={{ p: 1, display: "flex", justifyContent: "space-between" }}>
+                        <TextField variant='outlined' label="Moles" sx={{ maxWidth: "40%", marginX: "10px" }}
+                            value={moles}
+                            InputProps={{ endAdornment: <InputAdornment position="end">mole</InputAdornment> }} />
+
+                        <TextField variant='outlined' label="Volume of Solute" sx={{ maxWidth: "40%", marginX: "10px" }}
+                            value={soluteVolume}
+                            InputProps={{ endAdornment: <InputAdornment position="end">litre (l)</InputAdornment> }} />
+                    </Box>
+                )}
+            </Container>
+
+
         </Container>
     )
 }
